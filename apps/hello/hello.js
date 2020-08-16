5162b72f5fd05686e747d74190f3db937b80c3db
jeg <jugheadeatsalot@gmail.com>
2020-08-16 10:40:22 -0700

    ...

--- apps/hello/hello.js
+++ apps/hello/hello.js
@@ -4,12 +4,16 @@ var fs = require('fs');
 http.createServer(function (req, res) {
     if(req.url === '/index.html' || req.url === '/') {
         fs.readFile('./index.html', function(_err, data) {
+            if(_err) res.write(_err)
+
             res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': data.length});
             res.write(data);
             res.end();
         });
     } else {
         fs.readFile('./404.html', function(_err, data) {
+            if(_err) res.write(_err)
+
             res.writeHead(404, {'Content-Type': 'text/html', 'Content-Length': data.length});
             res.write(data);
             res.end();
