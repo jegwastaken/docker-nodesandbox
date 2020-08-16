cb078e7d228aaacc0dcfd1e85bd964c4f2624980
jeg <jugheadeatsalot@gmail.com>
2020-08-16 10:33:02 -0700

    ...

--- apps/hello/hello.js
+++ apps/hello/hello.js
@@ -3,14 +3,14 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
     if(req.url === '/index.html' || req.url === '/') {
-        fs.readFile('./index.html', function(_err, data) {
+        fs.readFileSync('./index.html', function(_err, data) {
             res.write(data);
             res.end();
         });
     } else {
         res.statusCode = 404;
 
-        fs.readFile('./404.html', function(_err, data) {
+        fs.readFileSync('./404.html', function(_err, data) {
             res.write(data);
             res.end();
         });
